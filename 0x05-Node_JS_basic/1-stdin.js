2454478dea2aa9848fa260f80185d1267ac6a51a
Awopeju Kolawole Moses <Awopejumoses@gmail.com>
2024-11-28 21:32:02 +0100

    fixed three

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -1,19 +1,14 @@
 process.stdout.write('Welcome to Holberton School, what is your name?\n');
 
+process.stdin.on('readable', () => {
+  const chunk = process.stdin.read();
 
-if (process.stdin.isTTY) {
-  process.stdin.on('data', (data) => {
-    process.stdout.write(`Your name is: ${data.toString()}`);
-    process.exit();
-  });
-} else {
-  process.stdin.on('data', (data) => {
-    process.stdout.write(`Your name is: ${data.toString()}`);
-    process.exit();
-  });
-  process.on('exit', () => {
-    process.stdout.write('This important software is now closing\n');
-  });
-}
+  if (chunk) {
+    process.stdout.write(`Your name is: ${chunk}`);
+  }
+});
 
+process.stdin.on('end', () => {
+  process.stdout.write('This important software is now closing\n');
+});
 
